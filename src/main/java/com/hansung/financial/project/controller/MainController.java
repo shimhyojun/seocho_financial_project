72137a51169c67ae201d2ba05c4def0b94f6d40c
Dowon Lee <edowon0623@gmail.com>
2021-09-27 22:09:59 +0900

    upload init sources

---
+++ src/main/java/com/hansung/financial/project/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.hansung.financial.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class MainController {
+    @RequestMapping("/")
+    public String dashboard() {
+        return "index";
+    }
+
+}
