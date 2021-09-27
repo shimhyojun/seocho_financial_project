783b7926bbe91ce14bdac24ba87a46c6be59a6ce
Dowon Lee <edowon0623@gmail.com>
2021-09-27 22:53:25 +0900

    upload

--- src/main/java/com/hansung/financial/project/controller/MainController.java
+++ src/main/java/com/hansung/financial/project/controller/MainController.java
@@ -1,20 +1,24 @@
 package com.hansung.financial.project.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class MainController {
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String dashboard() {
         return "index";
     }
 
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String test() {
         return "index";
     }
 
-    @RequestMapping("/test2")
-    public String test2() {
-        return "index";
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
