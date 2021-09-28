6d4d36eac80c996033a6100e50f59e7a9e9159b7
shimhyojun <83755070+shimhyojun@users.noreply.github.com>
2021-09-28 17:29:12 +0900

    upload shim code

--- src/main/java/com/hansung/financial/project/controller/MainController.java
+++ src/main/java/com/hansung/financial/project/controller/MainController.java
@@ -12,14 +12,39 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("/test")
+    @GetMapping("/buttons")
     public String test() {
-        return "index";
+        return "buttons";
     }
 
+    @GetMapping("/cards")
+    public String cards() { return "cards"; }
+
+    @GetMapping("/charts")
+    public String charts() { return "charts"; }
+
+    @GetMapping("/forgot-password")
+    public String forgot_password() { return "forgot-password"; }
+
     @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
+    public String login() { return "login"; }
+
+    @GetMapping("/register")
+    public String register() { return "register"; }
+
+    @GetMapping("/tables")
+    public String tables() { return "tables"; }
+
+    @GetMapping("/utilities-animation")
+    public String utilities_animation() { return "utilities-animation"; }
+
+    @GetMapping("/utilities-border")
+    public String utilities_border() { return "utilities-border"; }
+
+    @GetMapping("/utilities-color")
+    public String utilities_color() { return "utilities-color"; }
+
+    @GetMapping("/utilities-other")
+    public String utilities_other() { return "utilities-other"; }
+
 }
-aa
