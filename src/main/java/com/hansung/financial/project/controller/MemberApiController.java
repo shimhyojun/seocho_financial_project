f3089bac5e163d668110f357be8c58b4480934bb
edowon0623@gmail.com <Dwlee$09>
2021-10-04 16:03:17 +0900

    added a sample code to register

--- src/main/java/com/hansung/financial/project/controller/MemberApiController.java
+++ src/main/java/com/hansung/financial/project/controller/MemberApiController.java
@@ -2,6 +2,8 @@ package com.hansung.financial.project.controller;
 
 import com.hansung.financial.project.entity.Member;
 import com.hansung.financial.project.service.MembersService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
